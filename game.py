9d36b1195b5dae3aed4491775e5595b33f0de8dc
rishabh <rishabh7890@gmail.com>
2019-08-18 10:59:24 -0400

    marker/mark fix

--- game.py
+++ game.py
@@ -27,7 +27,7 @@ def assign_marker():
   while marker != 'X' and marker != 'O':
     marker = input('Player 1 please choose X or O: ')
   # if statement for player 2's marker
-  # return markerers as tuple so we can later unpack
+  # return markers as tuple so we can later unpack
   if marker == 'X':
     print(('X', 'O'))
     return ('X', 'O')
@@ -35,28 +35,27 @@ def assign_marker():
     print(('O', 'X'))
     return ('O', 'X')
 
-# test assign_marker function
 # use tuple unpacking so we can print each choice indv later on if we need to
 player1_marker, player2_marker = assign_marker()
 # test player 1 marker which should be unpacked due to above statement
 # print(player1_marker)
 
-# function which takes in board as object, marker, and desired position on board and assigns it to the board
-def place_marker(board, marker, position):
-  # go to specified position on board and place marker there
-  board[position] = marker
+# function which takes in board as object, mark, and desired position on board and assigns it to the board
+def place_marker(board, mark, position):
+  # go to specified position on board and place mark there
+  board[position] = mark
 
 # test to see if place_marker works
 # place_marker(test_board, '$', 8)
 # display_board(test_board)
 
-# Function that takes in board and marker and then checks to see if that marker has won the game
+# Function that takes in board and mark and then checks to see if that mark has won the game
 # this will return True if a win condition is met , False if not
-def win_check(board, marker):
+def win_check(board, mark):
   # check to see different ways to win 
-  # EACH ROW, EACH COLLUMN, EACH DIAGONAL, and check to see if they all share same markerer
-  # remember marker is what you passed in before
-  (board[1] == board[2] == board[3] == marker) or (board[4] == board[5] == board[6] == marker) or (board[7] == board[8] == board[9] == marker) or (board[1] == board[4] == board[7] == marker) or (board[2] == board[5] == board[8] == marker) or (board[3] == board[6] == board[9] == marker) or (board[1] == board[5] == board[9] == marker) or (board[3] == board[5] == board[7] == marker)
+  # EACH ROW, EACH COLLUMN, EACH DIAGONAL, and check to see if they all share same mark
+  # remember mark is what you passed in before
+  (board[1] == board[2] == board[3] == mark) or (board[4] == board[5] == board[6] == mark) or (board[7] == board[8] == board[9] == mark) or (board[1] == board[4] == board[7] == mark) or (board[2] == board[5] == board[8] == mark) or (board[3] == board[6] == board[9] == mark) or (board[1] == board[5] == board[9] == mark) or (board[3] == board[5] == board[7] == mark)
 
 # use random module to create function to decide who goes first
 def choose_first():
@@ -95,7 +94,7 @@ def player_choice(board):
   # while position is not in the list of possible playable positions or space isnt free
   while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
     # ask the user to input a position and convert to int
-    position = int(input('Choose a available position: (1-9)'))
+    position = int(input('Choose an available position: (1-9)'))
   # return the new position choice for later use 
   return position
 
